b0311fef4f93c7be1ce4190176a811db75abd05f
AlexeyKukrus <kukrus.agz@gmail.com>
2024-12-21 12:50:46 +0300

    03: update config files for monorepo setup (tsconfig and eslint) + add axios instance

--- client/src/app/providers/setupAxios.ts
+++ client/src/app/providers/setupAxios.ts
@@ -1,3 +1,36 @@
-import axios from 'axios'
+import axios from 'axios';
 
-export default axios
+const api = axios.create({
+  baseURL: 'https://localhost:3001',
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      console.error('Пользователь не авторизован!');
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
